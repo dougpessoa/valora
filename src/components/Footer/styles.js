f89df1112f63d50d9cffdd48dba2b72fe8cfe731
Douglas Pessoa <douglaspereira1@outlook.com>
2020-09-16 21:38:55 -0300

    updtate

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -5,19 +5,19 @@ import { AiFillCopyrightCircle } from '../../styles/icons';
 export const Container = styled.div`
   width: 100%;
   height: 60px;
-  /* background: red; */
   padding: 20px 20px 20px 40px;
 
   display: flex;
   align-items: center;
 
-  font-size: 19px;
-  font-weight: 600;
+  font-size: 14px;
+  font-weight: 400;
   border-top: 2px solid rgba(0, 0, 0, 0.15);
 `;
 
 export const CopyIcon = styled(AiFillCopyrightCircle)`
-  width: 23px;
-  height: 23px;
+  width: 21px;
+  height: 21px;
   color: gray;
+  margin-right: 7px;
 `;
